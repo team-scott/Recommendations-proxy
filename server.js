71e9f15d90c94540708db72ae39c935d4b00e618
Alan Zhao <alan.zhao908@gmail.com>
2019-04-12 10:43:01 -0400

    done

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/:room', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public'));
+});
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
+
+
